30ad8d931e24352161491b37e27745876140a3bf
Hugh Do <mhughdo@gmail.com>
2020-11-19 00:35:10 +0700

    :heavy_plus_sign: add: typescript + chakra ui

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from 'next/app'
+import { ChakraProvider, CSSReset } from '@chakra-ui/react'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
+
+export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import { Box } from '@chakra-ui/react'
+
+export default function Home() {
+  return (
+    <Box bg='tomato' w='100%' p={4} color='white'>
+      This is the Box
+    </Box>
+  )
+}
