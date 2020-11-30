03c3f0a7de175110e6d25422df154069b4ecc40c
Hugh Do <mhughdo@gmail.com>
2020-12-01 00:09:24 +0700

    :sparkles: add: sign-in screen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,20 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider, CSSReset } from '@chakra-ui/react'
+import { Provider } from 'next-auth/client'
 import theme from '../theme/styles'
-import "../theme/date-picker-custom.css"
+import '../theme/date-picker-custom.css'
 import '../theme/font.css'
-import 'leaflet/dist/leaflet.css';
+import 'leaflet/dist/leaflet.css'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <CSSReset />
-      <Component {...pageProps} />
+      <Provider session={pageProps.session}>
+        <CSSReset />
+        <Component {...pageProps} />
+      </Provider>
     </ChakraProvider>
   )
 }
