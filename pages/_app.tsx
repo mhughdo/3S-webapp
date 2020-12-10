b019014a7727c6477a58a1534dd9b1ee4b2be749
Hugh Do <mhughdo@gmail.com>
2020-12-10 21:43:31 +0700

    :sparkles: home gamen to home api wo kuawase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider, CSSReset } from '@chakra-ui/react'
 import { Provider } from 'next-auth/client'
 // eslint-disable-next-line import/no-named-as-default-member
+import { QueryCache, ReactQueryCacheProvider } from 'react-query'
 import theme from '../theme/styles'
 import '../theme/date-picker-custom.css'
 import 'react-dates/initialize'
@@ -12,13 +13,17 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import '../assets/styles/pagination.css'
 
+const queryCache = new QueryCache()
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Provider session={pageProps.session}>
-        <CSSReset />
-        <Component {...pageProps} />
-      </Provider>
+      <ReactQueryCacheProvider queryCache={queryCache}>
+        <Provider session={pageProps.session}>
+          <CSSReset />
+          <Component {...pageProps} />
+        </Provider>
+      </ReactQueryCacheProvider>
     </ChakraProvider>
   )
 }
